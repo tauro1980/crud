e4da077fbd3e8dcfce29b68e1e88054b55767ff4
Luis Santos <luissantoszun24@hotmail.com>
2021-02-09 08:13:29 -0500

    Adicionando una tarea y mostrarla en la lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { isEmpty } from 'lodash'
 import React, { useState } from 'react'
+import shortid from 'shortid'
 
 function App() {
   const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
 
   const addTask = (e) => {
     e.preventDefault()
@@ -11,7 +13,13 @@ function App() {
       return
     }
 
-    console.log("ok")
+    const newTask = {
+      id: shortid.generate(),
+      name: task
+    }
+
+    setTasks([ ...tasks, newTask ])
+
     setTask("")
   }
 
@@ -23,11 +31,15 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de Tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
-              <span className="lead">Nombre de la tarea</span>
-              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-              <button className="btn btn-warning btn-sm float-right">Editar</button>
-            </li>
+            {
+              tasks.map((task) =>(
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-right">Editar</button>
+                </li>
+              ))
+            }
           </ul>
         </div>
         <div className="col-4">
